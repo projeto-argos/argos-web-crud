85ad0d419206746d05ba547ae95682d33a8d7400
Cansani <lucca0cansani@gmail.com>
2026-08-14 13:10:46 -0300

    feat: adicionar estrutura inicial da aplicação

---
+++ src/main/java/model/Admin.java
@@ -0,0 +1,67 @@
+package model;
+
+public class Admin {
+
+    // ATRIBUTOS
+    private String cpf;
+    private String nome;
+    private String email;
+    private String telefone;
+    private boolean ativo;
+
+    // METODOS CONSTRUTORES
+
+    public Admin() {
+    }
+    public Admin(String cpf, String nome, String email, String telefone, boolean ativo) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.ativo = ativo;
+    }
+
+    // METODOS GETTERS
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public boolean getAtivo() {
+        return ativo;
+    }
+
+    // METODOS SETTERS
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    // SAIDA FORMATADA
+    @Override
+    public String toString(){
+        return "CPF: " + this.cpf +
+                "\nNome: " + this.nome +
+                "\nE-mail: " + this.email +
+                "\nTelefone: " + this.telefone;
+    }
+}
---
+++ src/main/java/model/EstoqueMedicamento.java
@@ -0,0 +1,112 @@
+package model;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class EstoqueMedicamento {
+
+    // ATRIBUTOS
+    private UUID idEstoqueMedicamento;
+    private LocalDate dataValidade;
+    private String status;
+    private String localArmazenamento;
+    private LocalDate dataFabricacao;
+    private String loteFabricacao;
+    private LocalDate dataEntrega;
+    private int qtdDisponivel;
+    private LocalDateTime atualizadoEm;
+    private UUID idMedicamento;
+    private boolean ativo;
+
+    // METODOS CONSTRUTORES
+    public EstoqueMedicamento() {
+    }
+
+    public EstoqueMedicamento(UUID idEstoqueMedicamento, LocalDate dataValidade, String status, String localArmazenamento, LocalDate dataFabricacao, String loteFabricacao, LocalDate dataEntrega, int qtdDisponivel, LocalDateTime atualizadoEm, UUID idMedicamento, boolean ativo) {
+        this.idEstoqueMedicamento = idEstoqueMedicamento;
+        this.dataValidade = dataValidade;
+        this.status = status;
+        this.localArmazenamento = localArmazenamento;
+        this.dataFabricacao = dataFabricacao;
+        this.loteFabricacao = loteFabricacao;
+        this.dataEntrega = dataEntrega;
+        this.qtdDisponivel = qtdDisponivel;
+        this.atualizadoEm = atualizadoEm;
+        this.idMedicamento = idMedicamento;
+        this.ativo = ativo;
+    }
+
+    //  METODOS GETTERS
+    public UUID getIdEstoqueMedicamento() {
+        return idEstoqueMedicamento;
+    }
+
+    public LocalDate getDataValidade() {
+        return dataValidade;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getLocalArmazenamento() {
+        return localArmazenamento;
+    }
+
+    public LocalDate getDataFabricacao() {
+        return dataFabricacao;
+    }
+
+    public String getLoteFabricacao() {
+        return loteFabricacao;
+    }
+
+    public LocalDate getDataEntrega() {
+        return dataEntrega;
+    }
+
+    public int getQtdDisponivel() {
+        return qtdDisponivel;
+    }
+
+    public LocalDateTime getAtualizadoEm() {
+        return atualizadoEm;
+    }
+
+    public UUID getIdMedicamento() {
+        return idMedicamento;
+    }
+
+    public boolean getAtivo() {
+        return ativo;
+    }
+
+
+
+    // METODOS SETTERS
+    public void setQtdDisponivel(int qtdDisponivel) {
+        this.qtdDisponivel = qtdDisponivel;
+    }
+
+    public void setLocalArmazenamento(String localArmazenamento) {
+        this.localArmazenamento = localArmazenamento;
+    }
+
+    // SAIDA FORMATADA
+
+
+    @Override
+    public String toString() {
+        return "ID do Estoque do Medicamento: " + this.idEstoqueMedicamento +
+                "\nData de Validade: " + this.dataValidade +
+                "\nStatus: " + this.status +
+                "\nLocal de Armazenamento: " + this.localArmazenamento +
+                "\nData de Fabricação: " + this.dataFabricacao +
+                "\nLote de Fabricação: " + this.loteFabricacao +
+                "\nData de Entrega: " + this.dataEntrega +
+                "\nQuantidade Disponível: " + this.qtdDisponivel +
+                "\nAtualizado Em: " + this.atualizadoEm +
+                "\nID do Medicamento: " + this.idMedicamento;
+    }
+}
