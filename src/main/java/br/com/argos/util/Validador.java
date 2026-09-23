b907098e3b4e82e75f76dccffb28d099f5d9c7aa
Murilo Paiva <murilo17.paiva@gmail.com>
2026-09-23 19:48:20 -0300

    (REGEX) Adicionando CNPJ e USUARIO na classe Validador

--- src/main/java/br/com/argos/util/Validador.java
+++ src/main/java/br/com/argos/util/Validador.java
@@ -7,6 +7,15 @@ public class Validador {
     private static final Pattern PADRAO_CPF = Pattern.compile("^\\d{11}$");
     private static final Pattern PADRAO_TELEFONE = Pattern.compile("^\\d{10,11}$");
     private static final Pattern PADRAO_EMAIL = Pattern.compile("^[\\w.+-]+@[\\w-]+\\.[a-zA-Z]{2,}$");
+    private static final Pattern PADRAO_CNPJ = Pattern.compile("^\\d{14}$");
+    private static final Pattern PADRAO_USUARIO = Pattern.compile("^[\\w.]{5,20}$");
+
+    // ================== PESOS CNPJ ==================
+    private static final int[] PESOS_CNPJ_PRIMEIRO =
+            {5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2};
+
+    private static final int[] PESOS_CNPJ_SEGUNDO =
+            {6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2};
 
     // ================== FORMATO (REGEX) ==================
 
@@ -25,6 +34,13 @@ public class Validador {
         return PADRAO_EMAIL.matcher(email).matches();
     }
 
+    public static boolean usuarioValido(String usuario){
+        if (usuario == null) {
+            return false;
+        }
+        return PADRAO_USUARIO.matcher(usuario).matches();
+    }
+
     // ================== CPF (FORMATO + DÍGITO VERIFICADOR) ==================
 
     public static boolean cpfValido(String cpf) {
@@ -38,7 +54,7 @@ public class Validador {
             return false;
         }
 
-        if (todosDigitosIguais(cpfLimpo)) {
+        if (todosDigitosIguaisCpf(cpfLimpo)) {
             return false;
         }
 
@@ -65,9 +81,59 @@ public class Validador {
         return (resto == 10) ? 0 : resto;
     }
 
-    private static boolean todosDigitosIguais(String cpf) {
+    private static boolean todosDigitosIguaisCpf(String cpf) {
         for (int i = 1; i < cpf.length(); i++) {
             if (cpf.charAt(i) != cpf.charAt(0)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // ================== CNPJ (FORMATO + DÍGITO VERIFICADOR) ==================
+
+    public static boolean cnpjValido(String cnpj){
+        if(cnpj == null){
+            return false;
+        }
+
+        String cnpjLimpo = cnpj.replaceAll("[^0-9]", "");
+
+         if (!PADRAO_CNPJ.matcher(cnpjLimpo).matches()){
+            return false;
+        }
+
+         if(todosDigitosIguaisCnpj(cnpjLimpo)) {
+             return false;
+         }
+
+        int primeiroDigito = calcularDigitoCnpj(cnpjLimpo, 12, PESOS_CNPJ_PRIMEIRO);
+         String cnpjParaSegundoDigito = cnpjLimpo.substring(0,12) + primeiroDigito;
+
+        int segundoDigito = calcularDigitoCnpj(cnpjParaSegundoDigito, 13, PESOS_CNPJ_SEGUNDO);
+
+        int digitoInformado1 = Character.getNumericValue(cnpjLimpo.charAt(12));
+        int digitoInformado2 = Character.getNumericValue(cnpjLimpo.charAt(13));
+
+        return primeiroDigito == digitoInformado1 && segundoDigito == digitoInformado2;
+
+    }
+
+    private static int calcularDigitoCnpj(String cnpj, int quantidadeDigitos, int[] pesos) {
+        int soma = 0;
+
+        for (int i = 0; i < quantidadeDigitos; i++) {
+            int numero = Character.getNumericValue(cnpj.charAt(i));
+            soma += numero * pesos[i];
+        }
+
+        int resto = soma % 11;
+        return (resto < 2) ? 0 : (11 - resto);
+    }
+
+    private static boolean todosDigitosIguaisCnpj(String cnpj) {
+        for (int i = 1; i < cnpj.length(); i++) {
+            if (cnpj.charAt(i) != cnpj.charAt(0)) {
                 return false;
             }
         }
